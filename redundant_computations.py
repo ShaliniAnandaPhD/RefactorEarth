6f3416e99696396eaf2ab19cafa54504df25d795
Shalini Ananda <50239203+ShaliniAnandaPhD@users.noreply.github.com>
2024-06-27 11:38:53 -0700

    Update redundant_computations.py

--- redundant_computations.py
+++ redundant_computations.py
@@ -6,6 +6,6 @@ def calculate_areas(radii):
         area = math.pi * radius * radius
         areas.append(area)
     return areas
-
+    
 radii = [1, 2, 3, 4, 5]
 circle_areas = calculate_areas(radii)
